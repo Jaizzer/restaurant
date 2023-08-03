f19e3e212f3f424e3e22470dbe087d0d587ee2ae
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-08-03 09:02:05 +0800

    chore: Add html-webpack-lugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
 const path = require('path');
+const HtmlWebpackpllugin = require('html-webpack-plugin')
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
+    plugins: [
+        new HtmlWebpackpllugin({
+            title: 'Restaurant',
+            template: './src/index-template.html',
+        }),
+    ],
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
