63010d0c9035e6a5a3e88459b7b9c1fa53f3b1ed
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-08-03 09:48:05 +0800

    chore: Configure Webpack to use style and css loader to css files

--- webpack.config.js
+++ webpack.config.js
@@ -19,4 +19,12 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
 };
