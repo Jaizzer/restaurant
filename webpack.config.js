c30fbb8aed4d6d591066502ed8519eb05d52bb28
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-08-03 09:39:46 +0800

    fix: Correct typo in 'devtool' property

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackpllugin = require('html-webpack-plugin')
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
-    devTool: 'inline-source-map',
+    devtool: 'inline-source-map',
     devServer: {
         static: './dist',
     },
