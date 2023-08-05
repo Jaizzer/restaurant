cb41a79c9fa705755229c645c61697051c85e570
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-08-05 09:42:31 +0800

    feat: Add menu buttons and functionality to load corresponding dishes base on type

--- src/renderMenu.js
+++ src/renderMenu.js
@@ -6,4 +6,19 @@ export function renderMenu() {
     // Create menu container.
     const menu = document.createElement("menu");
     menu.classList.add("menu");
+
+    // Render dish category buttons that shows corresponding selections when clicked.
+    const foodCategories = ["Classic", "Dessert", "Drinks", "Ramen", "Rolls", "Sushi and Sashimi", "Salad"];
+    foodCategories.forEach(foodCategory => {
+
+        // Create button.
+        let button = document.createElement("button");
+        button.textContent = `${foodCategory}`;
+
+        // Load correesponding dishes base on type/category when clicked.
+        button.addEventListener("click", () => showSelection(`${foodCategory}`));
+
+        // Append button to the container.
+        menu.appendChild(button);
+    })
 }
