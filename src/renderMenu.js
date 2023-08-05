3c7b66ec1b37aac6465ec30582117a5b849143d5
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-08-05 09:32:06 +0800

    feat: Add initial declarations in render menu module

---
+++ src/renderMenu.js
@@ -0,0 +1,9 @@
+export function renderMenu() {
+
+    // Access content.
+    const content = document.querySelector("#content");
+
+    // Create menu container.
+    const menu = document.createElement("menu");
+    menu.classList.add("menu");
+}
