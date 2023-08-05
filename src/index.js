97bc83ab95da5df938a1a33526d8d1f4a4f8e386
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-08-05 08:51:54 +0800

    feat: Add click functionality to 'Home' link

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+import { renderHome } from './renderHome';
 import './style.css';
+
+// Make home page link access home-page content when clicked.
+const home = document.querySelector(".home");
+home.addEventListener("click", renderHome);
