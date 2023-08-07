139d064dc57f2cf799f9e061d73d7bda0622f2fb
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-08-08 06:59:46 +0800

    feat: Add feature to 'menu' button in dashboard to load 'menu' when clicked

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import { renderHome } from './renderHome';
+import { renderMenu } from './renderMenu';
 import './style.css';
 
 // Make home page link access home-page content when clicked.
 const home = document.querySelector(".home");
 home.addEventListener("click", renderHome);
+
+const menu = document.querySelector(".menu");
+menu.addEventListener("click", renderMenu)
