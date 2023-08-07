e95bb8d59fa7a85353c9709d24544b23eb5e9206
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-08-08 06:18:25 +0800

    feat: Add 'images' object that dynamically imports all images in different directories and also return object containing images' information

--- src/handleMenuImages.js
+++ src/handleMenuImages.js
@@ -1,4 +1,31 @@
 /**
+ * Contains information (filename, dish name, dish price) about different images from different directories.
+ * 
+ * Each key in the this object corresponds to a specific directory, and the value associated with each key
+ * is an array of objects containing information about the images in that directory.
+ * 
+ * The "images" object structure may look like this:
+ * ```
+ * {
+ *    classics: [
+ *      { fileName: "pizza_15.jpg", dishName: "pizza", dishPrice: "15" },
+ *      { fileName: "burger_12.jpg", dishName: "burger", dishPrice: "12" },
+ *    ], // And so on...
+ * }
+ * ```
+ */
+export const images = {
+    classics: require.context('./images/classics/', true, /\.(jpg|png|svg|jpeg|gif)$/).keys().map(extractImageDetails),
+    desserts: require.context('./images/desserts/', true, /\.(jpg|png|svg|jpeg|gif)$/).keys().map(extractImageDetails),
+    drinks: require.context('./images/drinks/', true, /\.(jpg|png|svg|jpeg|gif)$/).keys().map(extractImageDetails),
+    ramen: require.context('./images/ramen/', true, /\.(jpg|png|svg|jpeg|gif)$/).keys().map(extractImageDetails),
+    rolls: require.context('./images/rolls/', true, /\.(jpg|png|svg|jpeg|gif)$/).keys().map(extractImageDetails),
+    sushiAndSashimi: require.context('./images/sushi-and-sashimi/', true, /\.(jpg|png|svg|jpeg|gif)$/).keys().map(extractImageDetails),
+    salad: require.context('./images/salad/', true, /\.(jpg|png|svg|jpeg|gif)$/).keys().map(extractImageDetails),
+};
+
+
+/**
  * Parse name of dish, price, and path from the 'path'.
  * Example: /dir1/dir2/images_10.png ====> { fileName: "images_10.png", dishName: "images", dishPrice: "10" }
  * 
