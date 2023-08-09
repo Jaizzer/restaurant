5f667430f778b36d7af39ef4190f4d35c1ae01f1
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-08-10 07:48:10 +0800

    feat: Add gallery-rendering module

---
+++ src/renderGallery.js
@@ -0,0 +1,24 @@
+import { deleteCurrentContent } from "./deleteCurrentContent.js";
+import { images } from "./handleImages.js";
+import { Card } from "./createCard.js";
+
+/**
+ * Render gallery images.
+ */
+export function renderGallery() {
+
+    // Delete current contents of "selection" container.
+    const content = document.querySelector("#content");
+    deleteCurrentContent(content);
+
+    const gallery = document.createElement("div");
+    gallery.classList.add("gallery2")
+    content.appendChild(gallery)
+    
+    // Create and append the cards containing dishes corresponding to "type".
+    images["gallery"].forEach(element => {
+        let dish = new Card("gallery", element.path, null, null, null);
+        gallery.appendChild(dish.create());
+    });
+}
+
