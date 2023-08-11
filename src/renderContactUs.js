4087dbc55dc091171614f236b9f6814e0840e5be
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-08-11 12:37:58 +0800

    feat: Combine contact-us card into one section

--- src/renderContactUs.js
+++ src/renderContactUs.js
@@ -1,52 +1,33 @@
 import { deleteCurrentContent } from "./deleteCurrentContent.js";
 import { Card } from "./createCard.js";
 
-
 export function renderContactUs() {
-    
+
     // Get content div.
     const content = document.querySelector("#content");
 
     // Delete previous content.
     deleteCurrentContent(content);
 
-    // Array to store all cards.
-    let cards = [];
-
-    // Create "Get in Touch" section.
-    let getInTouch = new Card(
-        "contact-us-section",
+    // Create a single card to combine all sections.
+    let combinedCard = new Card(
+        "contact-us",
         null,
-        "Get in Touch",
-        `We'd love to hear from you. Whether you have a question, feedback, or just want to say hello,
-        our team is ready to assist you. Reach out to us through the contact details below.`,
+        "Contact Us",
+        `
+        <p>
+            We'd love to hear from you. Whether you have a question, feedback, or just want to say hello,
+            our team is ready to assist you. Reach out to us through the contact details below.
+        </p>
+        <p>
+            Email: info@jaizzer.com <br><br> 
+            Phone: +1 (123) 456-7890 <br><br> 
+            Address: 123 Sushi Street, Tokyo, Japan
+        </p>
+        `,
         null
     );
-    cards.push(getInTouch);
 
-    // Create "Contact Information" section.
-    let contactInfo = new Card(
-        "contact-us-section",
-        null,
-        "Contact Information",
-        "Email: info@jaizzer.com <br> Phone: +1 (123) 456-7890 <br> Address: 123 Sushi Street, Tokyo, Japan",
-        null
-    );
-    cards.push(contactInfo);
-
-    // Create "Visit Us" section.
-    let visitUs = new Card(
-        "contact-us-section",
-        null,
-        "Visit Us",
-        `Come and experience the culinary journey at Jaizzer. We're conveniently located in the heart of
-         Tokyo. Reserve a table and treat yourself to an unforgettable dining adventure.`,
-        "Reserve a Table"
-    );
-    cards.push(visitUs);
-
-    // Append all cards to the content div.
-    cards.forEach((card) => {
-        content.appendChild(card.create());
-    });
+    // Append the combined card to the content div.
+    content.appendChild(combinedCard.create());
 }
